f377c404d7e510b85afe6858a791ded88f493aa9
Baihaqi Dzaky Rizkia <baihaqidr@gmail.com>
2026-09-07 08:13:38 +0700

    feat: add no-cache headers to app and implement new dashboard UI template

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 # Ensure UTF-8 output encoding for Windows terminal
 if sys.platform == 'win32':
